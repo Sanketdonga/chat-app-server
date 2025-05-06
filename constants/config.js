1ffdfce713a0c1c568965d2de17e3aa425307a0e
sanketdonga <sanketdongasanket@gmail.com>
2025-05-06 22:45:28 +0530

    Add note

--- constants/config.js
+++ constants/config.js
@@ -7,6 +7,11 @@ const corsOptions = {
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   };
+
+  // credentials: true :: when your frontend (on 5173/4173) sends requests to backend (like 3000), the browser is allowed to send cookies, auth tokens, etc.
+
+  //frontend 
+  //axios.get('http://localhost:3000/api/some-endpoint', { withCredentials: true });
   
   const CHATTU_TOKEN = "chattu-token";
   
